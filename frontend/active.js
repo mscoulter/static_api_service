7d3efa956e9bd77306499db84722f824843ca2a4
Michael Coulter <msc2086@gmail.com>
2016-07-04 21:34:45 -0600

    done

--- frontend/active.js
+++ frontend/active.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log("working");
-  var request = $.get("http://localhost:3000/users", function(data){
+  var request = $.get("http://localhost:3000/active_users", function(data){
     console.log(data);
     for (var i = 0; i < data.length; i++) {
       $('#names').append(data[i].user_data.first_name + " " + data[i].user_data.last_name + '<br>')
